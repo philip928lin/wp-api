203887b10ad48677218343bbdeb67164715425b0
YI-HAN CHEN <hannah51125@gmail.com>
2018-01-19 00:19:01 +0800

    change models

--- pictures/pictures/urls.py
+++ pictures/pictures/urls.py
@@ -1,7 +1,17 @@
 from django.conf.urls import url, include # add include as an import here
 from django.contrib import admin
+from rest_framework import routers
+from api import views
+router = routers.DefaultRouter()
+router.register(r'pics', views.PictureViewSet)
+router.register(r'features', views.FeatureViewSet)
+router.register(r'votes', views.VoteRecordViewSet)
 
+
+# router.register(r'groups', views.GroupViewSet)
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^api/', include('api.urls')) # add this line
+    url(r'^', include(router.urls)),
+    # url(r'^api/', include('api.urls')) # add this line
+    
 ]
